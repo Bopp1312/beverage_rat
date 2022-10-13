976283358538bdd2873db74e6b5eab484d58d225
Jonathan Bopp <jonathan.r.bopp@gmail.com>
2022-10-12 21:36:42 -0700

    Added transformation library

--- scripts/Aruco/aruco_constant.py
+++ scripts/Aruco/aruco_constant.py
@@ -6,26 +6,35 @@ from cv2 import aruco
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pandas as pd
+import denHartLib as dh
+from geometry_msgs.msg import PoseStamped
 
-from geometry_msgs import PoseStamped
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+parameters =  aruco.DetectorParameters_create()
 
-feet_to_meters = 39.37/12.0
-
+# Load camera calibration
 matrix_coef = np.load("../../Calibration/matrix_coefficents.npy")
 distortion_coef = np.load("../../Calibration/distortion_coefficents.npy")
+
 marker_length = 0.210 #m
 
-origins = {0: ()}
+
+# Transform from world origin to markers
+# World frame is defined as 0.5 meters infront of marker:0
+# with X+ of the world frame facing Marker:0
+TW_0 = dh.transformTranx(0.5) * dh.rotz(np.pi)
+
+print(TW_0)
+
 def main():
     cap = cv2.VideoCapture(2)
-    while(rospy.is_shutdown() == False):
+    while(not rospy.is_shutdown()):
         # Capture frame-by-frame
         ret, frame = cap.read()
         frame = cv2.resize(frame, (800,450),cv2.INTER_LINEAR)
         # Extract all information of tags from image
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
-        parameters =  aruco.DetectorParameters_create()
+
         corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters,cameraMatrix=matrix_coef,distCoeff=distortion_coef)
     
         #frame = (aruco.drawDetectedMarkers(frame.copy(), corners, ids))
@@ -47,6 +56,14 @@ def main():
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
+
+def toTransform(self,rVec,tVec):
+    transform = np.eye(4,4)
+    rotation, jac = cv2.Rodrigues(rVec)
+    position = tVec
+    transform[:3,:3] = rotation
+    transform[:3,3] = position
+    return np.matrix(transform) 
 
 if __name__ == "__main__":
     rospy.init_node("aruco_watcher",)
